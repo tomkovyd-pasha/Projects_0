bf9e94e3c171d4f82717408fd386d9380fd2ad00
tree_0_apples <tree_0_apples>
2021-03-22 22:00:11 +0200

    refactored all CurrencyExchangeRateProg selen.py

--- CurrencyExchangeRateProg/selen.py
+++ CurrencyExchangeRateProg/selen.py
@@ -1,17 +1,49 @@
 from selenium import webdriver
-import requests
 import ctypes
 
-URL = 'https://exchangerates.org.uk/USD-EUR-exchange-rate-history.html'
-driver = webdriver.Chrome()
-driver.set_window_position(-2000, 0)
-driver.get(URL)
 
-table_row = driver.find_elements_by_xpath('//*[@id="hd-maintable"]')
+class OpenFile:
+    def __init__(self, file_name: str, mode: str):
+        __tmp_file = open(file_name, 'r')
+        self.__tmp_file_content = __tmp_file.read()
+        __tmp_file.close()
+        self.__tmp_filename = file_name
+        try:
+            self.file_obj = open(file_name, mode)
+        except FileNotFoundError:
+            self.file_obj = open(file_name, 'w')
 
-data = [[td.text for td in row.find_elements_by_class_name('colone') + row.find_elements_by_class_name('coltwo')] for row in table_row][0]
-data_from_source = [f'USD/EUR,{a[-10:]},{a[a.find("USD = ") + 6:][:7]}' for a in data]
-data_from_source.sort()
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file_obj.close()
+        if exc_type is None:
+            return True
+        tmp_file = open(self.__tmp_filename, 'w')
+        tmp_file.write(self.__tmp_file_content)
+        tmp_file.close()
+        return True
+
+    def __enter__(self):
+        return self.file_obj
+
+
+class CurrencyExchange:
+    def __init__(self, this_currency: str, another_currency: str):
+        self.this_currency = this_currency
+        self.another_currency = another_currency
+        self.data_from_source: list
+        self.driver: webdriver
+
+    def create_url(self):
+        return f'https://exchangerates.org.uk/{self.this_currency}-{self.another_currency}-exchange-rate-history.html'
+
+    def open_driver_and_get_data(self):
+        self.driver = webdriver.Chrome()
+        self.driver.set_window_position(-2000, 0)
+        self.driver.get(self.create_url())
+        table_row = self.driver.find_elements_by_xpath('//*[@id="hd-maintable"]')
+        data = [[td.text for td in row.find_elements_by_class_name('colone') + row.find_elements_by_class_name('coltwo')] for row in table_row][0]
+        self.data_from_source = [f'{self.this_currency}/{self.another_currency},{a[-10:]},{a[a.find(f"{self.this_currency} = ") + 6:][:7]}' for a in data]
+        # self.data_from_source.sort()
 
 
 def create_str_from_list(lst_to_str: list) -> str:
@@ -23,14 +55,25 @@ def create_str_from_list(lst_to_str: list) -> str:
     return ''.join(map(lambda x: '\n' + x, lst_to_str))[1:]
 
 
-with open('ex.txt', 'r') as file:
-    data_in_file = list(map(lambda x: x.replace("\n", ""), file.readlines()))  # [file.readlines()]
-    data_in_file.sort()
+def refresh_data(currency_instance: CurrencyExchange, file_to_refresh_data: str):
+    currency_instance.open_driver_and_get_data()
+    with OpenFile(file_to_refresh_data, 'r') as file:
+        data_in_file = list(map(lambda x: x.replace("\n", ""), file.readlines()))
+        # data_in_file.sort()
 
-with open('ex.txt', 'w') as file:
-    result_lst = data_in_file + list(set(data_from_source) - set(data_in_file))
-    result_lst.sort()
-    file.write(create_str_from_list(result_lst))
+    with OpenFile(file_to_refresh_data, 'w') as file:
+        result_lst = data_in_file + list(set(currency_instance.data_from_source) - set(data_in_file))
+        # result_lst.sort()
+        file.write(create_str_from_list(result_lst))
 
-driver.quit()
-ctypes.windll.user32.MessageBoxW(0, "Готово", "Поновлення курсу валют", 1)
+    currency_instance.driver.quit()
+
+
+try:
+    usd_uah = CurrencyExchange('USD', 'UAH')
+    eur_uah = CurrencyExchange('EUR', 'UAH')
+    for i in (usd_uah, eur_uah):
+        refresh_data(i, 'ex.txt')
+    ctypes.windll.user32.MessageBoxW(0, 'Готово', 'Поновлення курсу валют', 1)
+except:
+    ctypes.windll.user32.MessageBoxW(0, 'Сталась помилка! Файл не змінено', 'Поновлення курсу валют', 1)
